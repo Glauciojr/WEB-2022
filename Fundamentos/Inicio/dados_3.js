554034c45b3ac1d47c6003b4fb75ccf8ba4182ff
Glaucio <glaucio.souza@fatec.sp.gov.br>
2022-02-22 16:51:27 +0000

    (22/02) Conceitos de váriaveis

---
+++ Fundamentos/Inicio/dados_3.js
@@ -0,0 +1,12 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578;
+console.log(typeof salario);
+
+let estachovendo = true // ou false
+console.log(typeof estachovendo);
+
+console.log(typeof "Teste");
+console.log(typeof 'teste');
---
+++ Fundamentos/Inicio/dasafio_area.js
@@ -0,0 +1,15 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592;
+let raio = 10;
+
+let areaCirc = PI * raio * raio
+
+console.log ("O valor da área é " + areaCirc + " m2.")
+
+console.log(Math.PI) // PI do JS
+
+let area = Math.PI * raio * raio
+
+console.log(area)
